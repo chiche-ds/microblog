c762670f282755c2f0ee2fd7116ccd7ceed8d125
chiche <chides2012@gmail.com>
2023-03-31 12:00:22 +0100

    fix  form validation to display error when empty

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -22,7 +22,12 @@ def index():
 def about():
     return "<h1>About Page </h1>" 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data
+        ))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
